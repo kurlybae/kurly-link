a66fa191543a652ddf3b24210e008e24b6f1796f
배창원 <changwon.bae@kurlycorp.com>
2023-09-16 00:58:00 +0900

    vercel edge functions 봇 추가

--- src/utils/is-robot.ts
+++ src/utils/is-robot.ts
@@ -6,9 +6,16 @@
 //   'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)', // 인스타그램
 //   'facebookexternalhit/1.1;line-poker/1.0', // 라인
 //   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/601.2.4 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.4 facebookexternalhit/1.1 Facebot Twitterbot/1.0', // 애플
+//   'Vercel Edge Functions' // vercel app
 
-const BOT_LIST = ['Slackbot', 'facebookexternalhit', 'Facebot', 'Twitterbot'];
+const BOT_LIST = [
+  /Slackbot/,
+  /facebookexternalhit/,
+  /Facebot/,
+  /Twitterbot/,
+  /^Vercel Edge Functions$/,
+];
 
 export function isRobot(useragent: string): boolean {
-  return BOT_LIST.some((x) => useragent.includes(x));
+  return BOT_LIST.some((x) => x.test(useragent));
 }
